8c0af522718443cb61018771b806eda52814d545
Jerilene Medrano <jerixmx@gmail.com>
2021-07-05 16:35:55 +0800

    Add login endpoint

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { register } = require('../../controllers/users');
+const { register, login } = require('../../controllers/users');
 
+// @route POST api/users/register
+// @desc Register user
+// @access Public
 router.route('/register').post(register);
+
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+router.route('/login').post(login);
