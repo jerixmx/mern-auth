a3d8452317b8e64093617b99bb81d53fee2bd4b3
Jerilene Medrano <jerixmx@gmail.com>
2021-07-05 12:30:05 +0800

    Add reference for options

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
+	return mongoose.connect(url, {
+		// Options for decprecation warnings
+		// https://mongoosejs.com/docs/deprecations.html
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	});
 };
 
-module.exports = connectDB
+module.exports = connectDB;
