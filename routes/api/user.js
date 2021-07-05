28dbc83dfd1e955fd9e719764c409db563eb10f0
Jerilene Medrano <jerixmx@gmail.com>
2021-07-05 15:57:31 +0800

    Create registration endpoint

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
 
-// Load input validation
-const validateRegistrationInput = require('../../validation/register');
-const validateLoginInput = require('../../validation/login');
+const { register } = require('../../controllers/users');
 
-// Load user model
-const User = require('../../models/User');
+router.route('/register').post(register);
