1c5f0becd21d2d90dbfd8e0132842ca4bec35e16
Jerilene Medrano <jerixmx@gmail.com>
2021-07-06 17:06:48 +0800

    Add Browser Router to enable Link use

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from './layout/Navbar';
 import Landing from './layout/Landing';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className='App'>
-			<Navbar />
-			<Landing />
+			<BrowserRouter>
+				<Navbar />
+				<Landing />
+			</BrowserRouter>
 		</div>
 	);
 }
