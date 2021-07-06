daf3a15b0182537dfc7c16121d92fdd8aa41bbfc
Jerilene Medrano <jerixmx@gmail.com>
2021-07-06 16:28:23 +0800

    Add landing and navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './layout/Navbar';
+import Landing from './layout/Landing';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Annyeong</h1>
+			<Navbar />
+			<Landing />
 		</div>
 	);
 }
